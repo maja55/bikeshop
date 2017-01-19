eab0c63ced6027175ea428c6c1c69ed54a46cb05
maja55 <majapodrug2@gmail.com>
2017-01-19 16:54:47 +0100

    Updated associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         
   has_one :profile
+  has_many :orders
 
   def has_profile?
     profile.present? && !profile.id.nil?

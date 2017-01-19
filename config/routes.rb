1f99def72425cd36595701ad21daa3919d45a2d0
StefanKollaart <garbera@icloud.com>
2017-01-19 18:51:08 +0100

    Working on session

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :users, only: [:show]
-  resources :profiles
+  resources :profiles, only: [:new, :edit, :create, :update]
   resources :orders
 
   get 'pages/home'
